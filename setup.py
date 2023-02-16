cb74f3182df5b042c30d557e3b59bf184e8f093d
benfa <benfa@KHALED>
2023-02-16 04:14:44 +0100

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Django-ecommerce',
-    version="1.1.20",
+    version="1.1.25",
     description='Django Ecommerce',
     author='Khaled Benfajria',
     author_email='benfajria.khaled11@gmail.com',
