6dc03010d5ada18b9adb9b6ca535399c451aad67
benfa <benfa@KHALED>
2023-02-16 01:45:00 +0100

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('version.txt') as f:
 
 setup(
     name='Django-ecommerce',
-    version='1.0.21',
+    version='1.0.23',
     description='Django Ecommerce',
     author='Khaled Benfajria',
     author_email='benfajria.khaled11@gmail.com',
