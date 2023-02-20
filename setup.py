629ec64d41ca3bdb92bba9c1986bd2b6c4201e0e
Khaled <benfajria.khaled11@gmail.com>
2023-02-20 06:56:25 -0500

    Bump version: 1.1.54 → 1.1.55

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Django-ecommerce',
-    version="1.1.54",
+    version="1.1.55",
     description='Django Ecommerce',
     author='Khaled Benfajria',
     author_email='benfajria.khaled11@gmail.com',
