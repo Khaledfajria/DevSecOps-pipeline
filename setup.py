43bfa06bf1468ea6700aa9c85431deea6c59e7b8
Jenkins User <jenkins@example.com>
2023-02-23 15:48:13 +0000

    Bump version: 1.1.236 → 1.1.237

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Django-ecommerce',
-    version="1.1.236",
+    version="1.1.237",
     description='Django Ecommerce',
     author='Khaled Benfajria',
     author_email='benfajria.khaled11@gmail.com',
