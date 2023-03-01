bcad3c0cc70bb56468ceff315572edc75372271c
jenkins <jenkins@zed.com>
2023-03-01 08:12:37 -0500

    Bump version: 1.1.324 → 1.1.325

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Django-ecommerce',
-    version="1.1.324",
+    version="1.1.325",
     description='Django Ecommerce',
     author='Khaled Benfajria',
     author_email='benfajria.khaled11@gmail.com',
